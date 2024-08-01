fc1a8188caaf1dbb6237646452ad8fa44c23d2c7
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-08-01 00:52:54 +0000

    second commit

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ let oneEuroIs = {
     "GBP": 0.87, // british pound
 };
 
+let oneDollarIs = {
+    "JPY": 149.69, // japan yen
+    "EURO": 0.92, // us dollar
+    "GBP": 0.78, // british pound
+};
+
+let oneYenIs = {
+    "EURO": 0.0062, // japan yen
+    "USD": 0.0067, // us dollar
+    "GBP": 0.0052, // british pound
+};
+
 // Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
     // Convertimos el valor a dólares
@@ -12,4 +24,14 @@ const fromEuroToDollar = function(valueInEuro) {
     return valueInDollar;
 };
 
-module.exports = { fromEuroToDollar, oneEuroIs};
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * oneDollarIs.JPY;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * oneYenIs.GBP;
+    return valueInPound; 
+}
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound, oneEuroIs, oneDollarIs, oneYenIs};
--- test.js
+++ test.js
@@ -1,12 +1,35 @@
-const { fromEuroToDollar, oneEuroIs} = require("./app.js");
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound, oneEuroIs, oneDollarIs, oneYenIs} = require("./app.js");
 
 test("One euro should be 1.07 dollars", function () {
   // Uso la función como debe ser usada
   const dollars = fromEuroToDollar(3.5);
 
   // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-  const expected = 3.5 * oneEuroIs.USD;
+  const expectedEuro = 3.5 * oneEuroIs.USD;
 
   // Hago mi comparación (la prueba)
-  expect(dollars).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+  expect(dollars).toBe(expectedEuro); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 });
+
+test("One dolar should be 149.69 yen", function () {
+    // Uso la función como debe ser usada
+    const yen = fromDollarToYen(3.5);
+  
+    // Si 1 dolar son 149.69 yen, entonces 3.5 dolares debe ser (3.5 * 149.69)
+    const expectedDollar = 3.5 * oneDollarIs.JPY;
+  
+    // Hago mi comparación (la prueba)
+    expect(yen).toBe(expectedDollar); // 1 dolar son 149.69 yen, entonces 3.5 dolares deberían ser = (3.5 * 149.69)
+  });
+
+  test("One yen should be 0.0052 pound", function () {
+    // Uso la función como debe ser usada
+    const pound = fromYenToPound(3.5);
+  
+    // Si 1 yen son 0.0052 pound, entonces 3.5 yen debe ser (3.5 * 0.0052)
+    const expectedYen = 3.5 * oneYenIs.GBP;
+  
+    // Hago mi comparación (la prueba)
+    expect(pound).toBe(expectedYen); // 1 yen son 0.0052 pound, entonces 3.5 yen deberían ser = (3.5 * 0.0052)
+  });
+
